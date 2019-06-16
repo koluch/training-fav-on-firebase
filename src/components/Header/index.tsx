653d09ffe69c4df67ce353a07ae649cc6d18bbb1
Nikolai Mavrenkov <koluch@koluch.ru>
2019-06-16 16:32:53 +0300

    Turn Header into functional component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import { Input, Button } from 'antd';
-import styles from "./index.module.less"
+import { Button, Input } from "antd";
+import styles from "./index.module.less";
 
-interface Props { value: string }
-
-export default class Header extends React.Component<Props, {}> {
-  render() {
-
-    return (
-      <div className={styles.root}>
-        <form className={styles.newUrlForm}>
-          <Input className={styles.newUrlFormControl} placeholder="URL" />
-          <Button className={styles.newUrlFormControl} htmlType="submit">Add</Button>
-        </form>
-      </div>
-    );
-  }
+export default function Header() {
+  return (
+    <div className={styles.root}>
+      <form className={styles.newUrlForm}>
+        <Input className={styles.newUrlFormControl} placeholder="URL"/>
+        <Button className={styles.newUrlFormControl} htmlType="submit">Add</Button>
+      </form>
+    </div>
+  );
 }
