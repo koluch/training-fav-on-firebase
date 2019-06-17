774815f7dfd05040afd7d71fe340185c4a950efe
Nikolai Mavrenkov <koluch@koluch.ru>
2019-06-17 16:41:38 +0300

    Improve layout, do not run subscription on every render

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,44 +4,48 @@ import Body from "../Body";
 import styles from "./index.module.less";
 import DataAccess from "../../DataAccess";
 import * as asyncResource from "../../asyncResource";
-import { AsyncResult, swch } from "../../asyncResource";
-import { TData, TNewItem } from "../../types";
-import { Spin } from "antd";
+import { AsyncResult } from "../../asyncResource";
+import { TData } from "../../types";
 
 interface Props {
-  dataAccess: DataAccess,
+  dataAccess: DataAccess;
 }
 
 export default function App(props: Props) {
-
   let initialDataState: AsyncResult<TData> = asyncResource.fetching();
-  const [newItem, setNewItem] = useState({ text: '', isFetching: false });
+  const [newItem, setNewItem] = useState({ text: "", isFetching: false });
   const [dataResult, setDataResult] = useState(initialDataState);
 
   useEffect(() => {
-    return props.dataAccess.subscribe((data) => {
-      setDataResult(asyncResource.success(data));
-    }, (error) => {
-      setDataResult(asyncResource.failed(error));
-    });
-  });
+    return props.dataAccess.subscribe(
+      data => {
+        setDataResult(asyncResource.success(data));
+      },
+      error => {
+        setDataResult(asyncResource.failed(error));
+      }
+    );
+  }, []);
 
   const handleAddItem = async () => {
+    setDataResult(asyncResource.fetching());
     setNewItem({ ...newItem, isFetching: true });
     await props.dataAccess.add({ url: newItem.text });
-    setNewItem({ ...newItem, text: '', isFetching: false });
+    setNewItem({ ...newItem, text: "", isFetching: false });
   };
 
   return (
     <div className={styles.root}>
-      <Header
-        newItemValue={newItem}
-        onChangeNewItemText={(text: string) => {
-          setNewItem({...newItem, text })
-        }}
-        onAddItem={handleAddItem}
-      />
-      <Body dataResult={dataResult} />,
+      <div className={styles.content}>
+        <Header
+          newItemValue={newItem}
+          onChangeNewItemText={(text: string) => {
+            setNewItem({ ...newItem, text });
+          }}
+          onAddItem={handleAddItem}
+        />
+        <Body dataResult={dataResult} />,
+      </div>
     </div>
   );
 }
